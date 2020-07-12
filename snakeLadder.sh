d6ee04b4f5c9125b7bdb0912cb9d9a2b0004c92e
aadityaawasthi9767 <aadityaawasthi9767@gmail.com>
2020-07-13 01:21:25 +0530

    [Aaditya]Refactored: Game Play

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -8,17 +8,18 @@ NO_PLAY=0
 LADDER=1;
 SNAKE=2;
 
-#ARRAY
-declare -a gameBoard[100];
-declare -a playerSteps[500];
+#ARRAYS
+declare -a playerSteps[];
 
 #VARIABLES;
 position=0;
 player=0;
 
 #echo "*****************************************************WELCOME TO SNAKE AND LADDERS***********************************";
-function playerMovement()
-{
+function playerMovement(){
+
+while [[ $player -lt $FINAL ]]
+do
 
 	diceRoll=$((RANDOM%6 + 1));
 	echo "Dice Roll:" $diceRoll;
@@ -30,17 +31,24 @@ function playerMovement()
 
 							$LADDER)
 										playerSteps[((position++))]=$diceRoll;
-										echo "Congratulations!! You encountered Ladder";
+										echo "Yaayyy!! You encountered Ladder";
 										echo "You will move up by: " $diceRoll;
 										player=$(($player + $diceRoll));
-										echo "New Position: " $player;;
+
+										if [[ $player -gt $FINAL ]]
+										then
+											player=100;
+										else
+											echo "New Position: " $player
+										fi;;
 
 							$SNAKE)
 										playerSteps[((position++))]=$diceRoll;
 										echo "Hisss!! You met Snake";
 										echo "You will move down by: "$diceRoll;
 										player=$(($player - $diceRoll));
-										if [[ $player -le 0 ]]
+
+										if [[ $player -lt 0 ]]
                               then
                                  player=0;
 											echo "New Position: " $player;
@@ -53,6 +61,19 @@ function playerMovement()
 										echo "Wrong Play";;
 
 		esac
+done
 }
 
+function playerWins(){
+	 if [[ $player -eq $FINAL ]]
+		then
+			 echo "**************************************************************************";
+          echo "You reached the 100";
+          echo "Congratulations! You WON.";
+    fi
+}
+#echo "number of steps: " ${#playerSteps[@]}
+
+
 playerMovement
+playerWins
