817a986f208d61b58072e69f278ee51b4494f9c8
aadityaawasthi9767 <aadityaawasthi9767@gmail.com>
2020-07-12 23:58:09 +0530

    [Aaditya]Added: Die Rolls

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,6 +1,14 @@
-#! /bin/bash -x
+#! /bin/bash 
 
 #CONSTANTS
 START=0;
 FINAL=100;
 PLAYER=1;
+echo "*****************************************************WELCOME TO SNAKE AND LADDERS***********************************";
+function diceThrows(){
+
+diceRoll=$((RANDOM%6 + 1));
+echo "Dice Roll:" $diceRoll;
+}
+
+diceThrows;
